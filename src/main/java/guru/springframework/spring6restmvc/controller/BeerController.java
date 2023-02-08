599d495a8c36583c1a6a2821d49332c3881fc13b
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-08 01:01:41 -0500

    Adding lombok logging example

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -1,14 +1,26 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 /**
  * @author Donald F. Coffin
  */
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 	private final BeerService beerService;
+
+	public Beer getBeerById(UUID id) {
+
+		log.debug("Get Beer by Id - in controller");
+
+		return beerService.getBeerById(id);
+	}
 }
