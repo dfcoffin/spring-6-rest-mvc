c1e52a35f97025f1e5e842bdb0d7ee6a1cd99047
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-08 00:36:20 -0500

    Adding constructor example

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Donald F. Coffin
+ */
+@AllArgsConstructor
+@Controller
+public class BeerController {
+	private final BeerService beerService;
+}
