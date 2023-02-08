55a8bd4619a143c45cce191f7f0965c4fc106a52
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-08 00:20:29 -0500

    Adding lombok pojo example

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6restmvc.model;
+
+/**
+ * @author Donald F. Coffin
+ */
+public enum BeerStyle {
+	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
