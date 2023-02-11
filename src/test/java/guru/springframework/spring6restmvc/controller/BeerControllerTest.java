13d3d2ca91e8b78de22a55556fc5be433b79c23d
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-11 01:08:26 -0500

    Adding mockito config

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
+import guru.springframework.spring6restmvc.service.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,9 +13,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author Donald F. Coffin
@@ -28,11 +31,17 @@ class BeerControllerTest {
 	@MockBean
 	BeerService beerService;
 
+	BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
 	@Test
 	void getBeerById() throws Exception {
+		Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+		given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
 
 		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
 				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON));
 	}
 }
