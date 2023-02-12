1c6221349c470e9f8ff7c9dc8df9e1f7f33f57b2
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-11 20:56:31 -0500

    Adding test of Patch beer

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -8,12 +8,15 @@ import guru.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +26,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.web.servlet.function.RequestPredicates.*;
 
 /**
  * @author Donald F. Coffin
@@ -42,9 +46,35 @@ class BeerControllerTest {
 
 	BeerServiceImpl beerServiceImpl;
 
+	@Captor
+	ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+	@Captor
+	ArgumentCaptor<Beer> beerArgumentCaptor;
+
 	@BeforeEach
 	void setUp() {
 		beerServiceImpl = new BeerServiceImpl();
+	}
+
+	@Test
+	void testPatchBeer() throws Exception {
+		Beer beer = beerServiceImpl.listBeers().get(0);
+
+		Map<String, Object> beerMap = new HashMap<>();
+		beerMap.put("beerName", "New Name");
+
+		mockMvc.perform(patch("/api/v1/beer/" + beer.getId())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(beerMap)))
+				.andExpect(status().isNoContent());
+
+		verify(beerService).patchBeerById(uuidArgumentCaptor.capture(), beerArgumentCaptor.capture());
+
+		assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+		assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue().getBeerName());
+
 	}
 
 	@Test
@@ -55,7 +85,6 @@ class BeerControllerTest {
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNoContent());
 
-		ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
 		verify(beerService).deleteById(uuidArgumentCaptor.capture());
 
 		assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
