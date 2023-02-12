3daa772e6874074904fd7f83c8a16108a0c71d7f
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-12 12:10:37 -0500

    refactor to DTOs

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring6restmvc.services;
 
-import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,12 +22,12 @@ import java.util.UUID;
 @Service
 public class BeerServiceImpl implements BeerService {
 
-	private Map<UUID, Beer> beerMap;
+	private Map<UUID, BeerDTO> beerMap;
 
 	public BeerServiceImpl() {
 		this.beerMap = new HashMap<>();
 
-		Beer beer1 = Beer.builder()
+		BeerDTO beer1 = BeerDTO.builder()
 				.id(UUID.randomUUID())
 				.version(1)
 				.beerName("Galaxy Cat")
@@ -39,7 +39,7 @@ public class BeerServiceImpl implements BeerService {
 				.updateDate(LocalDateTime.now())
 				.build();
 
-		Beer beer2 = Beer.builder()
+		BeerDTO beer2 = BeerDTO.builder()
 				.id(UUID.randomUUID())
 				.version(1)
 				.beerName("Crank")
@@ -51,7 +51,7 @@ public class BeerServiceImpl implements BeerService {
 				.updateDate(LocalDateTime.now())
 				.build();
 
-		Beer beer3 = Beer.builder()
+		BeerDTO beer3 = BeerDTO.builder()
 				.id(UUID.randomUUID())
 				.version(1)
 				.beerName("Sunshine City")
@@ -69,8 +69,8 @@ public class BeerServiceImpl implements BeerService {
 	}
 
 	@Override
-	public void patchBeerById(UUID beerId, Beer beer) {
-		Beer existing = beerMap.get(beerId);
+	public void patchBeerById(UUID beerId, BeerDTO beer) {
+		BeerDTO existing = beerMap.get(beerId);
 
 		if (StringUtils.hasText(beer.getBeerName())) {
 			existing.setBeerName(beer.getBeerName());
@@ -99,8 +99,8 @@ public class BeerServiceImpl implements BeerService {
 	}
 
 	@Override
-	public void updateBeerById(UUID beerId, Beer beer) {
-		Beer existing = beerMap.get(beerId);
+	public void updateBeerById(UUID beerId, BeerDTO beer) {
+		BeerDTO existing = beerMap.get(beerId);
 		existing.setBeerName(beer.getBeerName());
 		existing.setPrice(beer.getPrice());
 		existing.setUpc(beer.getUpc());
@@ -108,12 +108,12 @@ public class BeerServiceImpl implements BeerService {
 	}
 
 	@Override
-	public List<Beer> listBeers(){
+	public List<BeerDTO> listBeers(){
 		return new ArrayList<>(beerMap.values());
 	}
 
 	@Override
-	public Optional<Beer> getBeerById(UUID id) {
+	public Optional<BeerDTO> getBeerById(UUID id) {
 
 		log.debug("Get Beer by Id - in service. Id: " + id.toString());
 
@@ -121,8 +121,8 @@ public class BeerServiceImpl implements BeerService {
 	}
 
 	@Override
-	public Beer saveNewBeer(Beer beer) {
-		Beer savedBeer = Beer.builder()
+	public BeerDTO saveNewBeer(BeerDTO beer) {
+		BeerDTO savedBeer = BeerDTO.builder()
 				.id(UUID.randomUUID())
 				.version(2)
 				.createdDate(LocalDateTime.now())
--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring6restmvc.services;
 
-import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -13,10 +13,10 @@ import java.util.*;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-	private Map<UUID, Customer> customerMap;
+	private Map<UUID, CustomerDTO> customerMap;
 
 	public CustomerServiceImpl() {
-		Customer customer1 = Customer.builder()
+		CustomerDTO customer1 = CustomerDTO.builder()
 				.id(UUID.randomUUID())
 				.name("Michael Weston")
 				.version(1)
@@ -24,7 +24,7 @@ public class CustomerServiceImpl implements CustomerService {
 				.updateDate(LocalDateTime.now())
 				.build();
 
-		Customer customer2 = Customer.builder()
+		CustomerDTO customer2 = CustomerDTO.builder()
 				.id(UUID.randomUUID())
 				.name("Fionna Glenanne")
 				.version(1)
@@ -32,7 +32,7 @@ public class CustomerServiceImpl implements CustomerService {
 				.updateDate(LocalDateTime.now())
 				.build();
 
-		Customer customer3 = Customer.builder()
+		CustomerDTO customer3 = CustomerDTO.builder()
 				.id(UUID.randomUUID())
 				.name("Sam Axe")
 				.version(1)
@@ -48,8 +48,8 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
-	public void patchCustomerById(UUID customerId, Customer customer) {
-		Customer existing = customerMap.get(customerId);
+	public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+		CustomerDTO existing = customerMap.get(customerId);
 
 		if (StringUtils.hasText(customer.getName())) {
 			existing.setName(customer.getName());
@@ -62,15 +62,15 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
-	public void updateCustomerById(UUID customerId, Customer customer) {
-		Customer existing = customerMap.get(customerId);
+	public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+		CustomerDTO existing = customerMap.get(customerId);
 		existing.setName(customer.getName());
 	}
 
 	@Override
-	public Customer saveNewCustomer(Customer customer) {
+	public CustomerDTO saveNewCustomer(CustomerDTO customer) {
 
-		Customer savedCustomer = Customer.builder()
+		CustomerDTO savedCustomer = CustomerDTO.builder()
 				.id(UUID.randomUUID())
 				.version(1)
 				.createdDate(LocalDateTime.now())
@@ -84,12 +84,12 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
-	public Optional<Customer> getCustomerById(UUID id) {
+	public Optional<CustomerDTO> getCustomerById(UUID id) {
 		return Optional.of(customerMap.get(id));
 	}
 
 	@Override
-	public List<Customer> getAllCustomers() {
+	public List<CustomerDTO> getAllCustomers() {
 		return new ArrayList<>(customerMap.values());
 	}
 
