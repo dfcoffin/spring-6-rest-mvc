88ff7e83283199faf2ec981215e53b14993b17eb
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-26 22:17:10 -0500

    Completing assignment for customer

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -1,11 +1,14 @@
 package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,10 +31,77 @@ class CustomerControllerIT {
 	@Autowired
 	CustomerController customerController;
 
+	@Autowired
+	CustomerMapper customerMapper;
+
 	@Rollback
 	@Transactional
 	@Test
-	void testAllEmptyList() {
+	void deleteByIdFound() {
+		Customer customer = customerRepository.findAll().get(0);
+
+		ResponseEntity responseEntity = customerController.deleteCustomerById(customer.getId());
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+		assertThat(customerRepository.findById(customer.getId()).isEmpty());
+	}
+
+	@Test
+	void testDeleteByIdNotFound() {
+		assertThrows(NotFoundException.class, () -> {
+			customerController.deleteCustomerById(UUID.randomUUID());
+		});
+	}
+
+	@Test
+	void testUpdateNotFound() {
+		assertThrows(NotFoundException.class, () -> {
+			customerController.updateCustomerByID(UUID.randomUUID(), CustomerDTO.builder().build());
+		});
+	}
+
+	@Rollback
+	@Transactional
+	@Test
+	void updateExistingCustomer() {
+		Customer customer = customerRepository.findAll().get(0);
+		CustomerDTO customerDTO = customerMapper.customerToCustomerDto(customer);
+		customerDTO.setId(null);
+		customerDTO.setVersion(null);
+		final String customerName = "UPDATED";
+		customerDTO.setName(customerName);
+
+		ResponseEntity responseEntity = customerController.updateCustomerByID(customer.getId(), customerDTO);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+		Customer updatedCustomer = customerRepository.findById(customer.getId()).get();
+		assertThat(updatedCustomer.getName()).isEqualTo(customerName);
+	}
+
+	@Rollback
+	@Transactional
+	@Test
+	void saveNewCustomerTest() {
+		CustomerDTO customerDTO = CustomerDTO.builder()
+				.name("John Thompson")
+				.build();
+
+		ResponseEntity responseEntity = customerController.handlePost(customerDTO);
+
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(201));
+		assertThat(responseEntity.getHeaders().getLocation()).isNotNull();
+
+		String[] locationUUID = responseEntity.getHeaders().getLocation().getPath().split("/");
+		UUID savedUUID = UUID.fromString(locationUUID[4]);
+
+		Customer customer = customerRepository.findById(savedUUID).get();
+		assertThat(customer).isNotNull();
+	}
+
+	@Rollback
+	@Transactional
+	@Test
+	void testListAllEmptyList() {
 		customerRepository.deleteAll();
 		List<CustomerDTO> dtos = customerController.listAllCustomers();
 
@@ -46,7 +116,7 @@ class CustomerControllerIT {
 	}
 
 	@Test
-	void testCustomerIdNotFound() {
+	void testGetByIdNotFound() {
 		assertThrows(NotFoundException.class, () -> {
 			customerController.getCustomerById(UUID.randomUUID());
 		});
@@ -55,9 +125,7 @@ class CustomerControllerIT {
 	@Test
 	void testGetById() {
 		Customer customer = customerRepository.findAll().get(0);
-
 		CustomerDTO dto = customerController.getCustomerById(customer.getId());
-
 		assertThat(dto).isNotNull();
 	}
 }
