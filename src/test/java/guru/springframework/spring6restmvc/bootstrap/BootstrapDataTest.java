654a857ed9b8ff6f3a3ee5b7963daa44fc6997bd
dfcoffin <donald.coffin@reminetworks.com>
2023-03-02 20:03:16 -0600

    Fixing integration test

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -3,10 +3,12 @@ package guru.springframework.spring6restmvc.bootstrap;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import guru.springframework.spring6restmvc.services.BeerCsvService;
+import guru.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,6 +16,7 @@ import static org.assertj.core.api.Assertions.*;
  * @author Donald F. Coffin
  */
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
 	@Autowired
@@ -29,14 +32,14 @@ class BootstrapDataTest {
 
 	@BeforeEach
 	void setUp() {
-		bootstrapData = new BootstrapData(beerRepository, customerRepository);
+		bootstrapData = new BootstrapData(beerRepository, customerRepository, csvService);
 	}
 
 	@Test
 	void testRun() throws Exception{
 		bootstrapData.run(null);
 
-		assertThat(beerRepository.count()).isEqualTo(3);
+		assertThat(beerRepository.count()).isEqualTo(2413);
 		assertThat(customerRepository.count()).isEqualTo(3);
 	}
 }
