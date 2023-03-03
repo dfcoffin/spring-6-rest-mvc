0d72a43e5cb19bce1aeab121c9b1886df9c3edb3
dfcoffin <donald.coffin@reminetworks.com>
2023-03-02 21:51:27 -0600

    Adding query parameter

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -73,7 +74,7 @@ public class BeerController {
     }
 
     @GetMapping(value = BEER_PATH)
-    public List<BeerDTO> listBeers(){
+    public List<BeerDTO> listBeers(@RequestParam(required = false) String beerName){
         return beerService.listBeers();
     }
 
