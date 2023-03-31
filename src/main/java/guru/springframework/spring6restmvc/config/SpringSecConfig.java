8ef23706718df648d2216a2aa7d78c03573d85b3
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-03-31 00:21:51 -0400

    Adding SpringSecConfig class

---
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ */
+
+@Configuration
+public class SpringSecConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().ignoringRequestMatchers("/api/**");
+		return http.build();
+	}
+}
